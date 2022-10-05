159a62eca86985f95912ccb6e32f1754f29e6e7b
Solomon1732 <shlomireu5@gmail.com>
2022-10-06 02:15:08 +0300

    Format the main python file
    Formatted main python file using pre-commit.
    
    Reworded the module docstring of `recursive_hardlink.py`

--- RecursiveHardLink/recursive_hardlink.py
+++ RecursiveHardLink/recursive_hardlink.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""
+Run the main function.
+"""
 import argparse
 import os
 import shutil
 from pathlib import Path
+
 
 def resolve_parents(directory: Path) -> Path:
     """Resolve the parent directories of 'directory'."""
@@ -13,20 +17,36 @@ def resolve_parents(directory: Path) -> Path:
 
     return Path(parent, dir_name)
 
+
 def main() -> None:
     """Main function of the module."""
 
-    parser = argparse.ArgumentParser(description="Recursively hard link the directory 'source' and its content to 'dest'. If 'source' or the parent directories of 'dest' don't exist then the program exits with an error. By default symbolic links are not followed.")
-    parser.add_argument('-s', '--follow-symlinks', action='store_true', help="always follow symbolic links")
-    parser.add_argument('source', type=Path, help="source directory to be traversed")
-    parser.add_argument('dest', type=Path, help='the name of the directory to be created')
+    parser = argparse.ArgumentParser(
+        description=(
+            "Recursively hard link the directory 'source' and its content to 'dest'."
+            " If 'source' or the parent directories of 'dest' don't exist then the"
+            " program exits with an error. By default symbolic links are not followed."
+        ),
+    )
+    parser.add_argument(
+        "-s",
+        "--follow-symlinks",
+        action="store_true",
+        help="always follow symbolic links",
+    )
+    parser.add_argument("source", type=Path, help="source directory to be traversed")
+    parser.add_argument(
+        "dest", type=Path, help="the name of the directory to be created"
+    )
     args = parser.parse_args()
     follow_symlinks: bool = args.follow_symlinks
     source: Path = args.source.resolve(strict=True)
     dest: Path = resolve_parents(args.dest)
 
-    shutil.copytree(src=source, dst=dest, symlinks=follow_symlinks, copy_function=os.link)
+    shutil.copytree(
+        src=source, dst=dest, symlinks=follow_symlinks, copy_function=os.link
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
